a0b873d52b74b6db639703253783c9842f17320f
Adrian Hara <info@adrianhara.com>
2020-10-09 11:13:26 +0200

    Initial commit.

---
+++ src/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using InfiniSwiss.ChromiumHtmlToPdf;
+
+namespace Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sourceUrl = "https://developer.mozilla.org/en-US/docs/Web/CSS/@media";
+            var pdfFilePath = Path.Combine(Directory.GetCurrentDirectory(), "out.pdf");
+
+            var printer = new ChromiumPdfPrinter();
+            await printer.PrintToPdfAsync(sourceUrl, pdfFilePath);
+        }
+    }
+}
---
+++ src/InfiniSwiss.ChromiumHtmlToPdf/ChromiumPdfPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using InfiniSwiss.CdpSharp;
+
+namespace InfiniSwiss.ChromiumHtmlToPdf
+{
+    public class ChromiumPdfPrinter
+    {
+        public async Task PrintToPdfAsync(string sourceUrl, string pdfFilePath)
+        {
+            await PrintToPdfAsync(sourceUrl, pdfFilePath, new ChromiumExecutionOptions() {RunHeadless = true});
+        }
+
+        public async Task PrintToPdfAsync(string sourceUrl, string pdfFilePath, ChromiumExecutionOptions chromiumExecutionOptions)
+        {
+            using var chromiumExecutionContext = new ChromiumExecutionContext(chromiumExecutionOptions);
+            await chromiumExecutionContext.StartAsync();
+
+            var pageNavigationCommand = chromiumExecutionContext.Page.CreateNavigationCommand();
+            await pageNavigationCommand.ExecuteAsync(sourceUrl);
+
+            var pagePrintToPdfCommand = chromiumExecutionContext.Page.CreatePrintPdfCommand();
+            var base64Pdf = await pagePrintToPdfCommand.ExecuteAsync();
+
+            File.WriteAllBytes(pdfFilePath, Convert.FromBase64String(base64Pdf));
+        }
+    }
+}
