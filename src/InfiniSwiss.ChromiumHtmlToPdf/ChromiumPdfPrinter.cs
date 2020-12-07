3e403565ab8cb6f6f159c6c56b744a960ec0ef6a
Adrian Hara <info@adrianhara.com>
2020-12-07 15:27:57 +0100

    Updated CdpSharp reference and exposed PrintToPdfOptions

--- src/InfiniSwiss.ChromiumHtmlToPdf/ChromiumPdfPrinter.cs
+++ src/InfiniSwiss.ChromiumHtmlToPdf/ChromiumPdfPrinter.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using InfiniSwiss.CdpSharp;
+using InfiniSwiss.CdpSharp.Commands.Page.Printing;
 
 namespace InfiniSwiss.ChromiumHtmlToPdf
 {
@@ -12,7 +13,7 @@ namespace InfiniSwiss.ChromiumHtmlToPdf
             await PrintToPdfAsync(sourceUrl, pdfFilePath, new ChromiumExecutionOptions() {RunHeadless = true});
         }
 
-        public async Task PrintToPdfAsync(string sourceUrl, string pdfFilePath, ChromiumExecutionOptions chromiumExecutionOptions)
+        public async Task PrintToPdfAsync(string sourceUrl, string pdfFilePath, ChromiumExecutionOptions chromiumExecutionOptions, PrintToPdfOptions options = null)
         {
             using var chromiumExecutionContext = new ChromiumExecutionContext(chromiumExecutionOptions);
             await chromiumExecutionContext.StartAsync();
@@ -21,6 +22,22 @@ namespace InfiniSwiss.ChromiumHtmlToPdf
             await pageNavigationCommand.ExecuteAsync(sourceUrl);
 
             var pagePrintToPdfCommand = chromiumExecutionContext.Page.CreatePrintPdfCommand();
+
+            if (options != null)
+            {
+                pagePrintToPdfCommand.DisplayHeaderFooter = options.DisplayHeaderFooter;
+
+                if (!string.IsNullOrEmpty(options.HeaderTemplate))
+                {
+                    pagePrintToPdfCommand.HeaderTemplate = options.HeaderTemplate;
+                }
+
+                if (options.UseDefaultFooterTemplate || !string.IsNullOrEmpty(options.FooterTemplate))
+                {
+                    pagePrintToPdfCommand.FooterTemplate = options.UseDefaultFooterTemplate ? CdpPagePrintPdfCommand.DefaultFooterTemplate : options.FooterTemplate;
+                }
+            }
+            
             var base64Pdf = await pagePrintToPdfCommand.ExecuteAsync();
 
             File.WriteAllBytes(pdfFilePath, Convert.FromBase64String(base64Pdf));
---
+++ src/InfiniSwiss.ChromiumHtmlToPdf/PrintToPdfOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace InfiniSwiss.ChromiumHtmlToPdf
+{
+    public class PrintToPdfOptions
+    {
+        public bool DisplayHeaderFooter { get; set; }
+
+        public string HeaderTemplate { get; set; }
+        
+        public string FooterTemplate { get; set; }
+
+        public bool UseDefaultFooterTemplate { get; set; }
+    }
+}
